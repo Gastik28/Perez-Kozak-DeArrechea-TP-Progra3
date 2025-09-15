643a045069a46e90a45bd0a4a374ca20873c65ad
GastiK28 <gkozak@udesa.edu.ar>
2025-09-15 08:27:19 -0300

    Primeras instalaciones

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Switch,Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import "./styles.css";
+
+class Header extends Component {
+  render() {
+    return (
+    <nav>
+        <li> </li>
+        <ul> </ul>
+        <li> </li>
+    </nav>
+    )
+  }
+}
+
+export default Header
