5c20afd6c7f76fe0d84cecd04268ab5a50c459a8
AgustinDeArrechea <dearrecheaagustin@gmail.com>
2025-09-18 16:04:57 -0300

    Boton favoritos en pelis y series

--- src/components/PopularMovies/PopularMovies.js
+++ src/components/PopularMovies/PopularMovies.js
@@ -9,6 +9,7 @@ import './styles.css'
         <section className='section_popmovie'>
             {
                 this.props.movies.slice(0,4).map((elm,idx)=> <Card key={idx + elm.original_title} data={elm} css={'card-article-popular-movies'} type={'movie'}/> )
+                
             }
         </section>
     )
